d619fbf4972e52425f350764eb6185607b73052c
nam <trinhthanhnam2002@gmail.com>
2023-05-04 14:40:57 +0700

    Sửa giao diện Add Customer , định dạng DatePicker

--- src/main/java/Controller/AddProductController.java
+++ src/main/java/Controller/AddProductController.java
@@ -7,19 +7,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class AddProductController implements Initializable {
+    @FXML
+    private DatePicker datePicker_MFG;
+
+    @FXML
+    private DatePicker datePicker_EXP;
+
     @FXML
     private Button btn_back;
     @FXML
@@ -50,8 +60,58 @@ public class AddProductController implements Initializable {
         Image image = new Image(String.valueOf(filePath));
         image_product.setImage(image);
     }
+
+//    @FXML
+//    public void getDate(ActionEvent event){
+//        LocalDate myDate = datePicker_MFG.getValue();
+//        myDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+//    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        //Định dạng DatePicker thành dd/mm/yyyy
+        datePicker_MFG.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+        datePicker_EXP.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+
+        //SET SỰ KIỆN btn_back
             btn_back.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
