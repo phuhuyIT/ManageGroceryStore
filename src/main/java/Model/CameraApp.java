abad67c86d70fdd29c8de39ad1ca49c61065644a
phuhuykoxl <phuhuy02003@gmail.com>
2023-05-06 14:28:13 +0700

    Hoàn thiện tính năng quét mã Qr và update file sql

--- src/main/java/Model/CameraApp.java
+++ src/main/java/Model/CameraApp.java
@@ -1,35 +1,92 @@
 package Model;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicBoolean;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import Controller.AlertAndVerifyController;
 import com.google.zxing.*;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.oned.Code128Writer;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
 
-public class CameraApp {
-    public static String scan() throws Exception {
+
+public class CameraApp extends Thread{
+    private  String barCode;
+    private TextField tf_addProductUPC;
+    private final AtomicBoolean isRunning = new AtomicBoolean(false);
+    public String getBarcode() {
+        return barCode;
+    }
+    @Override
+    public void run() {
+
+        isRunning.set(true);
         OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(1); // truy cập camera laptop
-        grabber.start();
+        try {
+            grabber.start();
+
         CanvasFrame canvasFrame = new CanvasFrame("Barcode Scanner");
-        canvasFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        //canvasFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    isRunning.set(false);
+                    canvasFrame.dispose();
+                    try {
+                        grabber.close();
+                    } catch (FrameGrabber.Exception ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
         Result result = null;
         boolean isNotHasBarCode=true;
-        while (isNotHasBarCode) {
+        while (isRunning.get()) {
             Frame frame = grabber.grab();
             BufferedImage image = convertFrameToImage(frame);
             result = scanBarcode(image);
             if (result != null) {
-                System.out.println("Barcode: " + result.getText());
-                isNotHasBarCode=false;
-                canvasFrame.dispose();
+                barCode= result.getText();
+                Result finalResult = result;
+                Platform.runLater(() -> {
+                    tf_addProductUPC.setText(finalResult.getText());
+                    AlertAndVerifyController.informationAlert("Success","ĐÃ QUÉT THÀNH CÔNG");
+                });
+                Thread.sleep(10000);
+                System.out.println("Barcode: " + finalResult.getText());
+
             }
             canvasFrame.showImage(frame);
         }
-        return result.getText();
+        } catch (FrameGrabber.Exception e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void addWindowListener(WindowAdapter windowAdapter) {
     }
 
     private static BufferedImage convertFrameToImage(Frame frame) {
@@ -51,5 +108,44 @@ public class CameraApp {
             return null;
         }
     }
+    public static String skuGenerate(String barcodeData, String fileName) throws WriterException, IOException {
+        // Tạo mã vạch dưới dạng hình ảnh
+        Code128Writer barcodeWriter = new Code128Writer();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeData, BarcodeFormat.CODE_128, 300, 150, hints);
+
+        // Tạo hình ảnh mới chứa mã vạch và nội dung
+        BufferedImage barcodeImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+        // Tạo hình ảnh mới chứa cả mã vạch và nội dung
+        BufferedImage combined = new BufferedImage(400, 200, BufferedImage.TYPE_INT_RGB);
+
+        // Vẽ mã vạch và nội dung lên hình ảnh mới
+        Graphics2D g2d = (Graphics2D) combined.getGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, 400, 200);
+        g2d.drawImage(barcodeImage, 50, 10, null);
+        g2d.setColor(Color.BLACK);
+        g2d.setFont(new Font("Arial", Font.BOLD, 14));
+
+        // Tính toán vị trí của nội dung
+        int stringWidth = g2d.getFontMetrics().stringWidth(barcodeData);
+        int xCoordinate = (combined.getWidth() - stringWidth) / 2;
+        int yCoordinate = 170;
+        g2d.drawString(barcodeData, xCoordinate, yCoordinate);
+
+        // Lưu hình ảnh dưới dạng tập tin PNG
+        String filePath = "src/main/java/BarcodeImage/"+fileName;
+        File outputFile = new File(filePath);
+        ImageIO.write(combined, "PNG", outputFile);
+        return filePath;
+    }
+
+
+    public void getTextField(TextField tf_addProductUPC) {
+        this.tf_addProductUPC=tf_addProductUPC;
+    }
+
 }
 
