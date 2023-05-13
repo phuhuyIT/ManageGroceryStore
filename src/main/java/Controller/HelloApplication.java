8e8d18c0ffa29841f54c1fd5452ba6f46dce54ca
nam <trinhthanhnam2002@gmail.com>
2023-05-13 22:53:24 +0700

    Sửa Quantity

--- src/main/java/Controller/HelloApplication.java
+++ src/main/java/Controller/HelloApplication.java
@@ -3,23 +3,32 @@ package Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),620, 550);
+        Scene scene1 = new Scene(fxmlLoader.load(),620, 550);
         stage.setTitle("Log In");
-        stage.setScene(scene);
+        stage.setScene(scene1);
         stage.setResizable(false);
         stage.show();
+
+
     }
+
+
 
     public static void main(String[] args) {
 
         launch();
     }
+
 }
