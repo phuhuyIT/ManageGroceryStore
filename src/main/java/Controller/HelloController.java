711fbd139338dabb0ee63c49446063b79c0f071d
nam <trinhthanhnam2002@gmail.com>
2023-05-16 07:48:49 +0700

    Sửa khung giao diện LogIn, RegisForm,ForgotPass

--- src/main/java/Controller/HelloController.java
+++ src/main/java/Controller/HelloController.java
@@ -3,9 +3,15 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,13 +22,13 @@ public class HelloController implements Initializable {
     @FXML
     private Button btn_login;
     @FXML
-    private Button btn_loginGoogle;
-    @FXML
-    private Button btn_loginFB;
+    private ToggleButton tb_showHidePass;
     @FXML
     private Button btn_forgotpassword;
     @FXML
     public TextField tf_username;
+    @FXML
+    public TextField tf_showPass;
     @FXML
     public PasswordField tf_password;
     @Override
@@ -41,10 +47,42 @@ public class HelloController implements Initializable {
                 LoginController.changeScence1(event , "views/RegisForm.fxml", "Sign Up", null);
             }
         });
+
+        Image icon1 = new Image("D:\\workspace\\BTL_demo\\src\\main\\resources\\Controller\\image\\eye.png");
+        Image icon2 = new Image("D:\\workspace\\BTL_demo\\src\\main\\resources\\Controller\\image\\show.png");
+
+       // tb_showHidePass = new ToggleButton();
+        tb_showHidePass.setGraphic(new ImageView(icon1));
+
+        // Đăng ký một bộ lắng nghe sự kiện cho nút
+        tb_showHidePass.setOnAction(event -> {
+            if (tb_showHidePass.isSelected()) {
+                tb_showHidePass.setGraphic(new ImageView(icon2));
+                String pass = tf_password.getText();
+                tf_showPass.setText(pass);
+                tf_showPass.setVisible(true);
+                tf_password.setVisible(false);
+            } else {
+                tb_showHidePass.setGraphic(new ImageView(icon1));
+                tf_showPass.setVisible(false);
+                tf_password.setVisible(true);
+            }
+        });
         btn_forgotpassword.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                LoginController.changeScence1(event, "views/forgot-password.fxml", "Forgot pasword", null);
+                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/forgot-password.fxml"));
+                Stage stage = new Stage();
+                Scene scene = null;
+                try {
+                    scene = new Scene(fxmlLoader.load(),627, 431);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                stage.setTitle("Recovery Password");
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
             }
         });
     }
